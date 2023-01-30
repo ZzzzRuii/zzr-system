32c5cb1a6d4604ba9fb5655ea18c5f8c36eb9c69
ZhouZhiRui <zzr@foxhis.com>
2023-01-30 10:42:40 +0800

    资源信息文件修改 2023.1.30

--- zzr-system-tool/src/main/java/com/zzr/apollo/controller/WebGlobalExceptionHandler.java
+++ zzr-system-tool/src/main/java/com/zzr/apollo/controller/WebGlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.zzr.apollo.controller;
 
-import cn.hutool.core.util.CharsetUtil;
 import com.zzr.apollo.api.R;
 import com.zzr.apollo.api.ResultCode;
 import org.springframework.validation.BindingResult;
@@ -20,12 +19,12 @@ import java.util.Objects;
 @RestControllerAdvice
 public class WebGlobalExceptionHandler {
     /**
-     * 拦截JSON参数校验 消息编码转换 ISO_8859_1 -> UTF_8
+     * 拦截JSON参数校验
      */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public R validationException(MethodArgumentNotValidException e) {
         BindingResult bindingResult = e.getBindingResult();
         String defaultMessage = Objects.requireNonNull(bindingResult.getFieldError()).getDefaultMessage();
-        return R.fail(ResultCode.PARAM_VALID_ERROR, CharsetUtil.convert(defaultMessage, CharsetUtil.ISO_8859_1, CharsetUtil.UTF_8));
+        return R.fail(ResultCode.PARAM_VALID_ERROR, defaultMessage);
     }
 }
