2d59249a2c9707700da6da66251b46e58e1a613a
ZhouZhiRui <zzr@foxhis.com>
2023-02-06 17:47:20 +0800

    service服务异常统一返回 2023.2.6

--- zzr-system-tool/src/main/java/com/zzr/base/handler/WebGlobalExceptionHandler.java
+++ zzr-system-tool/src/main/java/com/zzr/base/handler/WebGlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.zzr.base.handler;
 
+import com.zzr.base.api.IResultCode;
 import com.zzr.base.api.R;
 import com.zzr.base.api.ResultCode;
+import com.zzr.base.exception.ServiceException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.Objects;
 
 /**
- * WebGlobalExceptionHandler
+ * 全局异常处理器
  *
  * @author ZhouZhiRui
  * @version 1.0
@@ -26,5 +28,17 @@ public class WebGlobalExceptionHandler {
         BindingResult bindingResult = e.getBindingResult();
         String defaultMessage = Objects.requireNonNull(bindingResult.getFieldError()).getDefaultMessage();
         return R.fail(ResultCode.PARAM_VALID_ERROR, defaultMessage);
+    }
+
+    /**
+     * 拦截Service服务异常
+     */
+    @ExceptionHandler(Exception.class)
+    public R serviceException(Exception e) {
+        if (e instanceof ServiceException) {
+            IResultCode resultCode = ((ServiceException) e).getResultCode();
+            return R.fail(resultCode);
+        }
+        return R.fail(ResultCode.UNKNOWN_ERROR, e.getMessage());
     }
 }
